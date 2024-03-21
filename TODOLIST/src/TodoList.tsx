21bccb675243386ed3741c67a717f11c4380446c
Patryk Ihas <97395198+ihasp@users.noreply.github.com>
2024-03-21 12:01:26 +0100

    Added data storing system

--- TODOLIST/src/TodoList.tsx
+++ TODOLIST/src/TodoList.tsx
@@ -1,65 +1,85 @@
-import React, {useState} from "react";
-import "bootstrap/dist/css/bootstrap.css"
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
-interface item{
-    id: number;
-    text: string;
-    completed: boolean;
+interface Item {
+  id: number;
+  text: string;
+  completed: boolean;
 }
 
 export const TodoList: React.FC = () => {
-   const [todos, setTodos] = useState<item[]>([
-       /* {id:1, text: "test1", completed: false},
-        {id:2, text: "test2", completed: false}
-        */
-    ])
-    
-    const [input, setInput] = useState<string>("");
+  const [todos, setTodos] = useState<Item[]>(() => {
+    const storedTodos = localStorage.getItem("todos");
+    return storedTodos ? JSON.parse(storedTodos) : [];
+  });
 
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
-    const handleToggle = (id:number) => {
-        setTodos(
-            todos.map((todo) => {
-                if(todo.id === id){
-                    return {...todo, completed: !todo.completed};
-                }
-                return todo;
-            })
-        )
-    }
+  const [input, setInput] = useState<string>("");
 
-    const handleClick = () =>{
-        const newTodo: item = {id: Date.now(), text:input, completed:false}
-        if (newTodo.text !== ""){
-            setTodos([...todos, newTodo]);
-            setInput("");
+  const handleToggle = (id: number) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
         }
+        return todo;
+      })
+    );
+  };
 
+  const handleClick = () => {
+    const newTodo: Item = { id: Date.now(), text: input, completed: false };
+    if (newTodo.text !== "") {
+      setTodos([...todos, newTodo]);
+      setInput("");
     }
+  };
 
-    return (
-      <div className="main-container">
-        <div>
-          <h1 style={{marginLeft:'30px'}}>TODO LIST</h1>
-          <ul>
-            {todos.map((todo)=>(
-                <li key={todo.id} onClick={()=> handleToggle(todo.id)}
-                  style={{textDecoration: todo.completed ? "line-through" : "none"}}>{todo.text}</li>
-            ))}
-          </ul>
+  return (
+    <div className="main-container">
+      <div>
+        <h1 style={{ textAlign: "center" }}>TODO LIST</h1>
+        <ul className="card">
+          {todos.map((todo) => (
+            <li
+              key={todo.id}
+              onClick={() => handleToggle(todo.id)}
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+            >
+              {todo.text}
+            </li>
+          ))}
+        </ul>
 
-
-          <div style={{maxWidth:'500px', marginTop: '20px', marginBottom:'10px', display: 'flex' }}>
-            <input  
-              type="text"
-              placeholder="Dodaj do listy"
-              className="form-control"
-              onChange={(e)=>setInput(e.currentTarget.value)}
-              value={input}
-            />
-          <button onClick={handleClick} className="btn btn-primary" style={{marginLeft:'5px'}}>Dodaj</button>
-           </div>
+        <div
+          style={{
+            maxWidth: "500px",
+            marginTop: "20px",
+            marginBottom: "10px",
+            display: "flex",
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Dodaj do listy"
+            className="form-control"
+            onChange={(e) => setInput(e.currentTarget.value)}
+            value={input}
+          />
+          <button
+            onClick={handleClick}
+            className="btn btn-primary"
+            style={{ marginLeft: "5px" }}
+          >
+            Dodaj
+          </button>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
