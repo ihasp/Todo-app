53ea336459a4b9dee781337ec8966e7e2162d082
Patryk Ihas <97395198+ihasp@users.noreply.github.com>
2024-04-08 15:21:59 +0200

    Further changes

--- TODOLIST/src/TodoList.tsx
+++ TODOLIST/src/TodoList.tsx
@@ -61,56 +61,51 @@ export const TodoList: React.FC = () => {
      };
 
   return (
-    <div className="grid w-screen h-screen justify-center">
-      <div>
-        <div className="">
-          <h1>TODO LIST</h1>
-        </div>
-        <Card>
-          <CardContent className="grid gap-2">
-            <CardTitle className="mt-4 ml-2 ">Zadania</CardTitle>
-            <div className="flex items-center space-x-4 rounded-md border p-2">
-              <BellIcon />
-              <p className="text-sm font-medium ml-1 mb-0 pr-6">
-                Twoje zadania na dziś
-              </p>
-            </div>
-            <Button
-              className=""
-              onClick={handleDeleteClick}
-              variant={"outline"}
-            >
-              Usuń wykonane
-            </Button>
-            <ul>
-              {todos.map((todo) => (
-                <li
-                  className="flex items-center space-x-4 rounded-md border p-4 mt-1 text-pretty"
-                  key={todo.id}
-                  onClick={() => handleToggle(todo.id)}
-                  style={{
-                    textDecoration: todo.completed ? "line-through" : "none",
-                    cursor: "pointer",
-                  }}
-                >
-                  {todo.text} - {formatDate(new Date(todo.id))}
-                </li>
-              ))}
-            </ul>
-          </CardContent>
-        </Card>
-
-        <div className="grid w-full gap-2 mt-5">
-          <Textarea
-            placeholder="Dodaj do listy"
-            className="max-h-2"
-            onChange={(e) => setInput(e.currentTarget.value)}
-            value={input}
-          />
-          <Button onClick={handleClick} variant={"outline"}>
-            Dodaj
+    <div className="grid w-screen h-screen justify-center"
+          style={{overflowX:"hidden"}}>
+      <Card className="mt-4">
+        <CardContent className="grid gap-2">
+          <CardTitle className="mt-4 ml-2 ">Zadania</CardTitle>
+          <div className="flex items-center space-x-4 rounded-md border p-2">
+            <BellIcon />
+            <p className="text-sm font-medium ml-1 mb-0 pr-6">
+              Twoje zadania na dziś
+            </p>
+          </div>
+          <Button className="" onClick={handleDeleteClick} variant={"outline"}>
+            Usuń wykonane
           </Button>
-        </div>
+          <ul>
+            {todos.map((todo) => (
+              <li
+                className="flex items-center space-x-4 rounded-md border p-4 mt-1"
+                key={todo.id}
+                onClick={() => handleToggle(todo.id)}
+                style={{
+                  textDecoration: todo.completed ? "line-through" : "none",
+                  cursor: "pointer"
+                }}
+              >
+                <p
+                  style={{maxWidth:"17em"}}>
+                  {todo.text} <br/> {formatDate(new Date(todo.id))}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </CardContent>
+      </Card>
+
+      <div className="grid w-full gap-2 mt-5 mb-5">
+        <Textarea
+          placeholder="Dodaj do listy"
+          className="max-h-2"
+          onChange={(e) => setInput(e.currentTarget.value)}
+          value={input}
+        />
+        <Button onClick={handleClick} variant={"outline"}>
+          Dodaj
+        </Button>
       </div>
     </div>
   );
