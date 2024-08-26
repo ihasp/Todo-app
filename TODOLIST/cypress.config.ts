e1e8db5893f93d794755fdca027a104580956577
Patryk Ihas <97395198+ihasp@users.noreply.github.com>
2024-08-26 17:39:38 +0200

    Cypress config changes

--- TODOLIST/cypress.config.ts
+++ TODOLIST/cypress.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("after:run", (results) => {
+        console.log(`Wynik testu: ${results}`);
+      });
+
+      return config;
     },
   },
 });
